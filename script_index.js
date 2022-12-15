7ef71310c359228ee4803120aedf4c6d9da25d67
Blosuhm <zakhar.kruptsala@gmail.com>
2022-12-15 22:21:06 +0000

    INIT

---
+++ script_index.js
@@ -0,0 +1,28 @@
+$(document).ready(function () {
+  // Define the ViewModel
+  function vm() {
+    console.log("ViewModel initiated...");
+    //---Variáveis locais
+    const self = this;
+    self.Coffees = ko.observableArray([]);
+    SaveData = function (id) {
+      // Save the id to local storage
+      localStorage.setItem("data", id);
+      console.log(localStorage.getItem("data"));
+      window.location = "pay.html";
+    };
+    $.ajax({
+      url: "coffee_data.json",
+      dataType: "json",
+      type: "GET",
+      success: function (data) {
+        console.log(data);
+        // Store the data in the view model
+        self.Coffees(data.coffees);
+        console.log(data.coffees);
+      },
+    });
+  }
+  // Create a new instance of the ViewModel and apply bindings
+  ko.applyBindings(new vm());
+});
---
+++ script_pay.js
@@ -0,0 +1,23 @@
+$(document).ready(function () {
+  // Define the ViewModel
+  function vm() {
+    console.log("ViewModel initiated...");
+    //---Variáveis locais
+    const self = this;
+    console.log(localStorage.getItem("data"));
+    self.Coffee = ko.observableArray();
+    $.ajax({
+      url: "coffee_data.json",
+      dataType: "json",
+      type: "GET",
+      success: function (data) {
+        console.log(data);
+        // Store the data in the view model
+        self.Coffee(data.coffees[localStorage.getItem("data")]);
+        console.log(data.coffees[localStorage.getItem("data")]);
+      },
+    });
+  }
+  // Create a new instance of the ViewModel and apply bindings
+  ko.applyBindings(new vm());
+});
