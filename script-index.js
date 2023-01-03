090078ed85669aafa0d7ea1354c322042e31da6c
Blosuhm <zakhar.kruptsala@gmail.com>
2023-01-03 23:05:37 +0000

    some small changes

--- script-index.js
+++ script-index.js
@@ -1,7 +1,8 @@
 $(document).ready(function () {
   // Define the ViewModel
-  function vm() {
+  function ViewModel() {
     const self = this;
+    self.error = ko.observable("");
     self.coffees = ko.observableArray([]);
     self.saveData = function (id) {
       // Save the id to local storage
@@ -9,50 +10,69 @@ $(document).ready(function () {
       window.location = "pay/pay.html";
     };
 
-    $.ajax({
-      url: "coffee_data.json",
-      dataType: "json",
-      type: "GET",
-      success: function (data) {
-        console.log(data);
-        // Store the data in the view model
-        self.coffees(data.coffees);
-        console.log(data.coffees);
-      },
+    ajaxHelper("coffee-data.json", "GET").done(function (data) {
+      console.log(data);
+      // Modify the data
+
+      data.coffees.forEach((item) => {
+        item.photo = `images/capsule-banner/${removeDiacritics(item.name)
+          .split(" ")
+          .join("-")
+          .toLowerCase()}.png`;
+        item.image = `images/capsule-picture/${removeDiacritics(item.name)
+          .split(" ")
+          .join("-")
+          .toLowerCase()}.png`;
+      });
+
+      // Store the data in the view model
+      self.coffees(data.coffees);
+      console.log(data.coffees);
     });
 
-    // if logged in
+    //* Iternal functions
+    function ajaxHelper(uri, method, data) {
+      self.error(""); // Clear error message
+      return $.ajax({
+        type: method,
+        url: uri,
+        dataType: "json",
+        contentType: "application/json",
+        data: data ? JSON.stringify(data) : null,
+        error: function (jqXHR, textStatus, errorThrown) {
+          console.log("AJAX Call[" + uri + "] Fail...");
+          hideLoading();
+          self.error(errorThrown);
+        },
+      });
+    }
+
+    function removeDiacritics(str) {
+      return str.normalize("NFD").replace(/[\u0300-\u036f]/g, "");
+    }
+
+    //! if logged in
     self.loggedIn = ko.observable(null);
     self.userName = ko.observable("");
     self.cart = ko.observableArray([]);
     self.logOut = function () {
-      $.ajax({
-        url: "logout",
-        type: "POST",
-        contentType: "application/json",
-        success: function (data) {
-          window.location.href = "index.html";
-        },
+      ajaxHelper("logout", "POST").done(function () {
+        window.location.href = "index.html";
       });
     };
 
-    $.ajax({
-      url: "server/data.json",
-      type: "GET",
-      contentType: "application/json",
-      success: function (data) {
-        self.loggedIn(data.loggedIn);
-        self.cart(data.cart);
-        if (self.loggedIn() != null) {
-          self.userName(data.accounts[self.loggedIn()].username);
-          self.cart(data.accounts[self.loggedIn()].cart);
-          console.log("Logged in");
-        }
-        console.log(data);
-      },
+    ajaxHelper("server/data.json", "GET").done(function (data) {
+      self.loggedIn(data.loggedIn);
+      self.cart(data.cart);
+      if (self.loggedIn() != null) {
+        self.userName(data.accounts[self.loggedIn()].username);
+        self.cart(data.accounts[self.loggedIn()].cart);
+        console.log("Logged in");
+      }
+      console.log(data);
     });
   }
-  ko.applyBindings(new vm());
+  ko.applyBindings(new ViewModel());
 
   $(window).scroll(function () {
     if ($(this).scrollTop() != 0) {
